63da52ef6e1e098a09bb43307df1c8f8ce3a6a0b
Imon Ovbude <imonovbude@gmail.com>
2019-02-23 11:22:35 -0800

    added port in env file to make it dynamic

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
 const server = require('./server');
 
-server.listen(4000, () => {
-    console.log('Server Running on http://localhost:4000 ')
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`Server Running on http://localhost:${port}`)
 });
